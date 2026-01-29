8403b7b61bbc4fe7bb9385d59a83575e8153de8f
BriceLucifer <2376671337@qq.com>
2026-01-29 14:27:22 +0800

    simulate ans gready lc-2027 when we accur the x we take 2 step further

--- c++/lc-2027.cc
+++ c++/lc-2027.cc
@@ -6,6 +6,14 @@ class Solution {
 public:
     int minimumMoves(string s) {
         const auto length = s.size();
-        if (length == 3) return 1;
+        auto ans {0};
+        for(int i = 0; i < length; i++){
+            if (s[i] == 'X'){
+                ans += 1;
+                i += 2;
+            }
+        }
+
+        return ans;
     }
 };
