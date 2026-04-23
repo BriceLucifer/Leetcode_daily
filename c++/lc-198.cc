4392bae0206fd5f9b9e7b35155bea794d4f1b3e3
BriceLucifer <2376671337@qq.com>
2026-04-23 18:23:53 +1200

    lc-983 is not done

--- c++/lc-198.cc
+++ c++/lc-198.cc
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <utility>
 #include <vector>
 
 using namespace std;
---
+++ c++/lc-983.cc
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+
+
+    }
+};
